ab241c4842aacae18cfc5a8bcfde2e2869cc0ebc
Adrian Madrid <aemadrid@gmail.com>
2011-12-16 18:18:06 -0700

    ...

--- lib/arjdbc/orientdb/connection_methods.rb
+++ lib/arjdbc/orientdb/connection_methods.rb
@@ -1,9 +1,10 @@
 class ActiveRecord::Base
   class << self
-    def orientdb_connection( config )
-      config[:port] ||= 2240
-      config[:url] ||= "jdbc:orient://#{config[:host]}:#{config[:port]}/#{ config[:database]}"
+    def orientdb_connection(config)
       config[:driver] ||= "com.orientechnologies.orient.jdbc.OrientJdbcDriver"
+      config[:port]   ||= 2240
+      config[:engine] ||= 'remote'
+      config[:url]    ||= "jdbc:orient:#{config[:engine]}:#{config[:host]}:#{config[:port]}/#{config[:database]}"
       jdbc_connection(config)
     end
   end
